360c43c1fbc9390f145a2a908b3b1c97b5d4de32
AtsukiTak <takatomgoo@gmail.com>
2017-10-04 20:11:54 +0900

    Change examples

---
+++ examples/broadcast_fibonacci.rs
@@ -0,0 +1,86 @@
+extern crate amqpr;
+extern crate futures;
+extern crate tokio_core;
+extern crate bytes;
+extern crate clap;
+
+use futures::{Sink, Stream};
+use tokio_core::reactor::{Core, Interval};
+
+use bytes::{BytesMut, BigEndian, BufMut};
+
+use clap::{App, Arg};
+
+use amqpr::broadcast_sink;
+
+use std::time::Duration;
+
+
+fn main() {
+    let (amqp_addr, user, pass) = get_args();
+
+    let mut core = Core::new().unwrap();
+
+    let bytes_stream = {
+        let fibonacci_stream =
+            futures::stream::unfold((0u64, 1u64), |(n0, n1)| Some(Ok((n0 + n1, (n1, n0 + n1)))));
+
+        let interval_stream = Interval::new(Duration::from_secs(1), &core.handle())
+            .unwrap()
+            .map_err(|e| println!("Error : {:?}", e));
+
+        let mut bytes_mut = BytesMut::new();
+
+        fibonacci_stream
+            .zip(interval_stream)
+            .map(move |(n,())| {
+            bytes_mut.clear();
+            bytes_mut.reserve(8);
+            bytes_mut.put_u64::<BigEndian>(n);
+            bytes_mut.clone().freeze()
+        })
+    };
+
+    let broadcast_sink = broadcast_sink(
+        "fibonacci".into(),
+        amqp_addr.parse().unwrap(),
+        user.into(),
+        pass.into(),
+        core.handle(),
+    ).sink_map_err(|_| ());
+
+    core.run(broadcast_sink.send_all(bytes_stream)).unwrap();
+}
+
+
+fn get_args() -> (String, String, String) {
+    let matches = App::new("AMQP fibonacci broadcaster")
+        .arg(
+            Arg::with_name("amqp_addr")
+                .short("a")
+                .long("amqp_addr")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("user")
+                .short("u")
+                .long("user")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("pass")
+                .short("p")
+                .long("pass")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+
+    (
+        matches.value_of("amqp_addr").unwrap().into(),
+        matches.value_of("user").unwrap().into(),
+        matches.value_of("pass").unwrap().into(),
+    )
+}
---
+++ examples/subscribe_and_print.rs
@@ -0,0 +1,82 @@
+extern crate amqpr;
+extern crate futures;
+extern crate tokio_core;
+extern crate bytes;
+extern crate clap;
+
+use futures::Stream;
+use tokio_core::reactor::Core;
+
+use clap::{App, Arg};
+
+use amqpr::subscribe_stream;
+
+fn main() {
+    let args = get_args();
+
+    let mut core = Core::new().unwrap();
+
+    let subscribe_stream = subscribe_stream(
+        args.exchange,
+        args.amqp_addr.parse().unwrap(),
+        args.user.into(),
+        args.pass.into(),
+        core.handle(),
+    );
+
+    let futures = subscribe_stream.for_each(|bytes| {
+        println!("{:?}", bytes);
+        Ok(())
+    });
+
+    core.run(futures).unwrap();
+}
+
+
+struct Args {
+    amqp_addr: String,
+    exchange: String,
+    user: String,
+    pass: String,
+}
+
+
+fn get_args() -> Args {
+    let matches = App::new("AMQP simple subscriber")
+        .arg(
+            Arg::with_name("amqp_addr")
+                .short("a")
+                .long("amqp_addr")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("amqp_exchange")
+                .short("e")
+                .long("amqp_exchange")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("user")
+                .short("u")
+                .long("user")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("pass")
+                .short("p")
+                .long("pass")
+                .required(true)
+                .takes_value(true),
+        )
+        .get_matches();
+
+    Args {
+        amqp_addr: matches.value_of("amqp_addr").unwrap().into(),
+        exchange: matches.value_of("amqp_exchange").unwrap().into(),
+        user: matches.value_of("user").unwrap().into(),
+        pass: matches.value_of("pass").unwrap().into(),
+    }
+}
